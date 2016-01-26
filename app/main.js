fbe8b2d8d58ee636f16e0020d53fcc066b638ae7
Chandu <Chandra.Vedantham@GMail.com>
2016-01-25 20:31:04 -0500

    Changed the import of RootElement

--- app/main.js
+++ app/main.js
@@ -1,8 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import * as components from './components.jsx' 
+import {RootElement}  from './components.jsx' 
 
-var RootElement = components.RootElement;
-
- 
 ReactDOM.render(<RootElement/>, document.getElementById('mount-point'));
